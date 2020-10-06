b00e4f028fe0bfb144b3c604ca2ad5d0da0b9419
Marcos Freitas <marcos.freitas@e-capital-sa.com.br>
2020-10-06 14:45:51 -0300

    First Commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,40 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allWordpressPost {
+        edges {
+          node {
+            title
+            content
+            date
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      
+      {data.allWordpressPost.edges.map(n => (
+        <div>
+          <h1> {n.node.title} </h1>
+          <p> {n.node.content} </p>
+          <p> {n.node.date} </p>
+        </div>
+      ))}
+
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default SecondPage
